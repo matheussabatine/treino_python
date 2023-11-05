1de8a88862201255961cc1aea11a481a22ee5b5d
matheussabatine <70068237+matheussabatine@users.noreply.github.com>
2023-11-05 19:07:14 -0300

    Add files via upload

---
+++ python.py
@@ -0,0 +1,54 @@
+#help()
+#explica como funciona os comandos, digitar no terminal (exemplo: print)
+
+
+#print(4+3+2)
+#imprime resultado no terminal
+
+# variável string
+#pais="ali"
+
+#type() determina o tipo de variável
+#print(type(pais))
+
+#variáveis
+#string
+#nome="pedro"
+
+#int
+#numero=4
+
+#float
+#numero=6.5
+
+'''
+int()
+converte o valor para int
+
+todo input é tratado como string, por isso devemos converter
+
+=
+atribui valor
+
+==
+compara igualdade
+'''
+
+'''
+valor=input("tente advinhar o valor")
+valor_convertido=int(valor)
+resposta=20
+if(valor_convertido == resposta):
+    print("correto")
+else:
+    print("errado é vinte")
+'''
+
+valor=input("digite o valor")
+valor_convertido=int(valor)
+if(valor_convertido%2==0):
+    print("par")
+else:
+    print("ímpar")
+
+
