fae3f54bb8d8aeda64df9766e323a49ef6804505
matheussabatine <70068237+matheussabatine@users.noreply.github.com>
2024-02-25 18:45:34 -0300

    Delete gui kivy directory

--- gui kivy/guia_documentacao/basico.py
+++
@@ -1,9 +0,0 @@
-from kivy.app import App
-from kivy.uix.label import Label
-
-class Calculadora(App):
-    def build(self):
-        return Label(text='Bom dia')
-    
-if __name__ == '__main__':
-    Calculadora().run()
--- gui kivy/livro/helloApp.py
+++
@@ -1,9 +0,0 @@
-from kivy.app import App
-from kivy.uix.button import Label
- 
-class HelloApp(App):
-    def build(self):
-        return Label()
-
-if __name__ == '__main__':
-    HelloApp().run()
